1f6528cf3ef9bfe46f9e14c237d637f247142fda
Shiffali <shiffali.grover@tothenew.com>
2015-11-23 11:38:28 +0530

    Initial Project Structure

---
+++ scf/bundle/src/main/java/com/social/cms/HelloService.java
@@ -0,0 +1,13 @@
+package com.social.cms;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
